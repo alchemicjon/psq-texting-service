af553210f261241857a5d47875fea9ee1b21f50f
Jon Richmond <jrichmond124@gmail.com>
2023-06-16 11:58:05 -0400

    enhancement: Add empty sms_service

---
+++ test/integration/sms_service_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class SmsServiceTest < ActionDispatch::IntegrationTest
+  def setup
+    @service = SmsService.new
+  end
+
+  test 'it runs' do
+    assert @service.call
+  end
+end
