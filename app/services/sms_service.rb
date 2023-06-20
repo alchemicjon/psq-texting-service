dfbe5ea8b347e4b8d55450e9f535f40efea94ccb
Jon Richmond <jrichmond124@gmail.com>
2023-06-20 11:34:00 -0400

    enhancement: Build SmsProvider and tests

--- app/services/sms_service.rb
+++ app/services/sms_service.rb
@@ -1,2 +1,45 @@
+require 'httparty'
+require 'pry'
+
 class SmsService
+  attr_reader :errors, :response
+
+  def initialize
+    @success = nil
+    @response = nil
+    @errors = []
+    @provider_uri = 'https://mock-text-provider.parentsquare.com/provider1'
+    @callback_url = 'https://example.com/delivery_status'
+    @options = { headers: { 'Content-Type' => 'application/json' } }
+  end
+
+  def call(message)
+    unless message.respond_to?(:phone_number) && message.respond_to?(:message_body)
+      @success = false
+      @errors.push 'You need to pass a message to SmsService'
+      return
+    end
+    body = {
+      to_number: message.phone_number,
+      message: message.message_body,
+      callback_url: @callback_url
+    }
+    @options[:body] = JSON.generate(body)
+    @response = HTTParty.post(
+      @provider_uri,
+      @options
+    )
+    unless @success = @response.success?
+      @errors.push @response.parsed_response
+    end
+    success?
+  end
+
+  def success?
+    !!@success
+  end
+
+  def failure?
+    !@success
+  end
 end
--- test/integration/sms_service_test.rb
+++ test/integration/sms_service_test.rb
@@ -3,9 +3,28 @@ require 'test_helper'
 class SmsServiceTest < ActionDispatch::IntegrationTest
   def setup
     @service = SmsService.new
+    @message = messages(:valid)
+    @provider_url = 'https://mock-text-provider.parentsquare.com/provider1'
+    stub_request(:any, @provider_url)
+      .to_return(status: 200, body: JSON.generate({ message_id: 'abc123' }))
   end
 
-  test 'it runs' do
-    assert @service.call
+  test 'without valid message it fails' do
+    assert_not(@service.call('blah'))
+    assert(@service.failure?)
+    assert_includes(@service.errors, 'You need to pass a message to SmsService')
+  end
+
+  test 'happy path: success is true' do
+    @service.call(@message)
+    assert(@service.success?)
+  end
+
+  test 'sad path: provider not available' do
+    stub_request(:any, @provider_url)
+      .to_return(status: 500, body: JSON.generate({ error: 'Something went wrong' }))
+    @service.call(@message)
+    assert(@service.failure?)
+    assert_includes(@service.errors, 'Something went wrong')
   end
 end
